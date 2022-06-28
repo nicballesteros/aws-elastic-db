25c54d16989ae252cb8a09ff378ae81b620bcc8b
Nic Ballesteros <nicballesteros2@gmail.com>
2022-06-28 16:25:47 -0400

    temp

--- ec2/cdk/bin/cdk.js
+++ ec2/cdk/bin/cdk.js
@@ -1,10 +1,10 @@
 #!/usr/bin/env node
-
+/*
 const cdk = require('aws-cdk-lib');
 const { CdkStack } = require('../lib/cdk-stack');
 
 const app = new cdk.App();
-new CdkStack(app, 'CdkStack', {
+new CdkStack(app, 'CdkStack', {*/
   /* If you don't specify 'env', this stack will be environment-agnostic.
    * Account/Region-dependent features and context lookups will not work,
    * but a single synthesized template can be deployed anywhere. */
@@ -18,4 +18,4 @@ new CdkStack(app, 'CdkStack', {
   // env: { account: '123456789012', region: 'us-east-1' },
 
   /* For more information, see https://docs.aws.amazon.com/cdk/latest/guide/environments.html */
-});
+//});
